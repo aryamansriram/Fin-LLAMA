16041a0396ed835e32adcd20106aa5a16a44ef08
Aryaman Sriram <aryamansriram@Aryamans-MacBook-Air-2.local>
2024-01-23 16:45:13 +0530

    Added streaming support

--- scripts/chat_app.py
+++ scripts/chat_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
-from hugchat import hugchat
-from hugchat.login import Login
-import random
+from llama_cpp import Llama
+from langchain_community.llms import LlamaCpp
+
 
 if 'user_responses' not in st.session_state:
     st.session_state['user_responses'] = []
@@ -26,6 +26,7 @@ def u_callback():
        st.session_state['user_responses'].append(
            st.session_state.user_input
        )
+       
 
 def b_callback():
     if st.session_state.user_input:
@@ -33,29 +34,44 @@ def b_callback():
            st.session_state.bot_input
        )
 
+llm = LlamaCpp(
+    model_path='models/fin_llama_q8.gguf',
+    temperature=0.75,
+    max_tokens=2000,
+    top_p=1,
+    )
+
+
 if __name__=="__main__":
-    st.title('Example Chat')
+    st.title('Llama-Chat')
     response = st.text_input('Enter your response',on_change=u_callback,key='user_input')
-    bot_response = st.text_input("Enter bot's response",on_change=b_callback,key='bot_input')
+    if len(st.session_state['bot_responses'])==1 and len(st.session_state['user_responses'])==0:
+        st.chat_message("ASSISTANT").write(st.session_state['bot_responses'][0])
     
-    
-    print("U: ",st.session_state['user_responses'])
-    print("B: ",st.session_state["bot_responses"])
     if st.session_state['user_responses'] or st.session_state['bot_responses']:
         k=0
         for i,(u,b) in enumerate(zip(st.session_state['user_responses'],
                         st.session_state['bot_responses'])):
-            if b:
-                st.chat_message("ASSISTANT").write(b)
-            if u:
-                st.chat_message("USER").write(u)
             
-            k+=1
+            st.chat_message("ASSISTANT").write(b)
+            st.chat_message("USER").write(u)
+          
 
-        if k<len(st.session_state['user_responses']):
-            st.chat_message("USER").write(st.session_state['user_responses'][k])
+    if response:
         
-        if k<len(st.session_state['bot_responses']):
-            st.chat_message("ASSISTANT").write(st.session_state['bot_responses'][k])
-    # print("U: ",st.session_state['user_responses'])
-    # print("B: ",st.session_state['bot_responses'])
+        bot_response = llm.stream(
+            '<s>[INST] '+response+' [/INST]'
+        )
+
+        bot_res = ''
+        with st.chat_message("ASSISTANT"):
+            placeholder = st.empty()
+            for chunk in bot_response:
+                bot_res = bot_res+chunk
+                placeholder.markdown(bot_res)
+
+        st.session_state["bot_responses"].append(bot_res)
+
+
+
+        
